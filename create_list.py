daa1a107af03ece3f34840e7ee8ed37116fe275b
Patrick Swackhammer <patrick@swackhammer.net>
2016-01-19 12:44:03 -0600

    Added yaml.dump printouts to screen

--- create_list.py
+++ create_list.py
@@ -1,3 +1,6 @@
+import yaml
+import json
+
 dict = {'ip_addr': '192.168.255.21', 'attribs': range(7)};
 list = range(8)
 list.append('dog')
@@ -6,3 +9,11 @@ list.append({})
 list[-1]=dict
 print list
 
+print 'yaml.dump(list)'
+print yaml.dump(list)
+
+print 'yaml.dump(list, default_flow_style=False)'
+print yaml.dump(list, default_flow_style=False)
+
+print 'yaml.dump(list, default_flow_style=True)'
+print yaml.dump(list, default_flow_style=True)
