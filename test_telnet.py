eb8c27ed34ed761d280fd0622eb4535290edf58a
Patrick Swackhammer <patrick@swackhammer.net>
2016-01-23 20:22:28 -0800

    Added test_telnet.py

---
+++ test_telnet.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import telnetlib
+
+TELNET_PORT = 23
+TELNET_TIMEOUT = 6
+
+def main():
+    ip_addr = '50.76.53.27'
+    username = 'pyclass'
+    password = '88newclass'
+
+    remote_conn = telnetlib.Telnet(ip_addr, TELNET_PORT, TELNET_TIMEOUT)
+    output = remote_conn.read_until("sername:", TELNET_TIMEOUT)
+    print output
+
+    remote_conn.close()
+
+if __name__ == "__main__":
+    main()
