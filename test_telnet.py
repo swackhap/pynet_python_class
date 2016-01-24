e521d7565d2d00439e3d2b283e0b5ef4e6ea780a
Patrick Swackhammer <patrick@swackhammer.net>
2016-01-23 20:52:01 -0800

    Updated test_telnet.py to login to router, do terminal length 0, and show version

--- test_telnet.py
+++ test_telnet.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import telnetlib
+import time
 
 TELNET_PORT = 23
 TELNET_TIMEOUT = 6
@@ -12,6 +13,22 @@ def main():
 
     remote_conn = telnetlib.Telnet(ip_addr, TELNET_PORT, TELNET_TIMEOUT)
     output = remote_conn.read_until("sername:", TELNET_TIMEOUT)
+    remote_conn.write(username + '\n')
+    output = remote_conn.read_until("ssword:", TELNET_TIMEOUT)
+
+    remote_conn.write(password + '\n')
+    time.sleep(1)
+    output = remote_conn.read_very_eager()
+    print output
+
+    remote_conn.write("terminal length 0" + '\n')
+    time.sleep(1)
+    output = remote_conn.read_very_eager()
+    print output
+
+    remote_conn.write("show version" + '\n')
+    time.sleep(1)
+    output = remote_conn.read_very_eager()
     print output
 
     remote_conn.close()
