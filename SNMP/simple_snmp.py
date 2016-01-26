a6a1e5625e9361464bc82585a58bbb9ffe495e59
Patrick Swackhammer <patrick@swackhammer.net>
2016-01-26 11:23:53 -0800

    Updated simple_snmp.py to connect to pynet-rtr1 and pynet-rtr2 and pretty-print MIB2 sysName and sysDescr

--- SNMP/simple_snmp.py
+++ SNMP/simple_snmp.py
@@ -1,16 +1,30 @@
 
 from snmp_helper import snmp_get_oid,snmp_extract
 
+#pynet-rtr1 = 50.76.53.27:7961
 COMMUNITY_STRING = 'galileo'
 SNMP_PORT = 7961
 IP = '50.76.53.27'
+OID_LIST = ('1.3.6.1.2.1.1.1.0','1.3.6.1.2.1.1.5.0')
 
 a_device = (IP, COMMUNITY_STRING, SNMP_PORT)
+print '-->Connecting to pynet-rtr1 at IP ' + a_device[0] + \
+    ' port ' + str(a_device[2]) + ' with community string ' + a_device[1] 
+for oid in OID_LIST:
+    snmp_data = snmp_get_oid(a_device, oid=oid)
+    output = snmp_extract(snmp_data)
+    print '\n OID ' + oid + ' value is \n' + output + '\n'
 
-OID = '1.3.6.1.2.1.1.1.0'
+#pynet-rtr2 = 50.76.53.27:8061
+COMMUNITY_STRING = 'galileo'
+SNMP_PORT = 8061
+IP = '50.76.53.27'
+OID_LIST = ('1.3.6.1.2.1.1.1.0','1.3.6.1.2.1.1.5.0')
 
-snmp_data = snmp_get_oid(a_device, oid=OID)
-
-output = snmp_extract(snmp_data)
-
-print output
+a_device = (IP, COMMUNITY_STRING, SNMP_PORT)
+print '-->Connecting to pynet-rtr2 at IP ' + a_device[0] + \
+    ' port ' + str(a_device[2]) + ' with community string ' + a_device[1] 
+for oid in OID_LIST:
+    snmp_data = snmp_get_oid(a_device, oid=oid)
+    output = snmp_extract(snmp_data)
+    print '\n OID ' + oid + ' value is \n' + output + '\n'
