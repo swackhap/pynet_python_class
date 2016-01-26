a896c7725ca474de56818617e4ebaf3da18be7b4
Patrick Swackhammer <patrick@swackhammer.net>
2016-01-26 10:55:03 -0800

    Added SNMP subdir and simple_snmp.py

---
+++ SNMP/simple_snmp.py
@@ -0,0 +1,16 @@
+
+from snmp_helper import snmp_get_oid,snmp_extract
+
+COMMUNITY_STRING = 'galileo'
+SNMP_PORT = 7961
+IP = '50.76.53.27'
+
+a_device = (IP, COMMUNITY_STRING, SNMP_PORT)
+
+OID = '1.3.6.1.2.1.1.1.0'
+
+snmp_data = snmp_get_oid(a_device, oid=OID)
+
+output = snmp_extract(snmp_data)
+
+print output
