82ae45da0df854979d7ae83fd131599907236100
Patrick Swackhammer <patrick@swackhammer.net>
2016-03-14 15:13:22 -0700

    Still working on exercise 4 with class creation

--- mytest/world.py
+++ mytest/world.py
@@ -1,6 +1,16 @@
 
-def func1():
-    print 'this is inside world.py'
+def func1(var1='VARIABLE1',var2='VARIABLE2',var3='VARIABLE3'):
+    print 'this is from func1 inside world.py'
+
+class MyClass(object):
+
+    def __init__(self,var1,var2,var3):
+        self.var1 = var1
+        self.var2 = var2
+        self.var3 = var3
+
+    def hello(self):
+        print "inside MyClass.hello, var1== " + self.var1 + ", var2== " + self.var2 + ", var3== " + self.var3
 
 def main():
     func1()
